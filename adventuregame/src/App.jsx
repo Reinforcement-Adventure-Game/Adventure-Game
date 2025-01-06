2374d3802b13086436c95e80bc0916bc986c50cf
elliesimens <elliesimens@gmail.com>
2025-01-06 17:10:39 -0500

    started the project with vite and set up a tailwind config file, we have a hello world page

---
+++ adventuregame/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <h1 className='text-3xl font-bold'>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
